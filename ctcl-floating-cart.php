c39ff19069dc1a7c533bd1972e881de29e9896f1
UjW0L <bktujwol@gmail.com>
2023-09-06 12:32:51 +0545

    Plugin header update

--- ctcl-floating-cart.php
+++ ctcl-floating-cart.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Ctcl Floating Cart
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.2.2
+ * Description:       CT Commerce Lite Floating cart block
+ * Requires at least: 6.3.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            UjW0L
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       ctcl-floating-cart
